43bd2eeda5719a92e9072772decc6a2df858aea1
Aaron A. King <kingaa@umich.edu>
2024-07-05 23:20:02 +0000

    version 1.9

--- R/subplex.R
+++ R/subplex.R
@@ -11,8 +11,7 @@
 ##'
 ##' @name subplex
 ##' @rdname subplex
-##' @include subplex-package.R
-##'
+##' @include package.R
 ##' @param par Initial guess of the parameters to be optimized over.
 ##' @param fn The function to be minimized.  Its first argument must be the
 ##' vector of parameters to be optimized over.  It should return a scalar
@@ -43,37 +42,20 @@
 ##' Algorithms'', Ph.D. thesis, Department of Computer Sciences, University of
 ##' Texas at Austin, 1990.
 ##' @keywords optimize
-##' @examples
+##' @example examples/ripple.R
+##' @example examples/rosen.R
 ##'
-##' rosen <- function (x) {   ## Rosenbrock Banana function
+##' @tests
+##'
+##' expect_silent({
+##' rosen <- function (x) {
 ##'   x1 <- x[1]
 ##'   x2 <- x[2]
 ##'   100*(x2-x1*x1)^2+(1-x1)^2
 ##' }
-##' subplex(par=c(11,-33),fn=rosen)
-##'
-##' rosen2 <- function (x) {
-##'   X <- matrix(x,ncol=2)
-##'   sum(apply(X,1,rosen))
-##' }
-##' subplex(par=c(-33,11,14,9,0,12),fn=rosen2,control=list(maxit=30000))
-##' ## compare with optim:
-##' optim(par=c(-33,11,14,9,0,12),fn=rosen2,control=list(maxit=30000))
-##'
-##' ripple <- function (x) {
-##'   r <- sqrt(sum(x^2))
-##'   1-exp(-r^2)*cos(10*r)^2
-##' }
-##' subplex(par=c(1),fn=ripple,hessian=TRUE)
-##' subplex(par=c(0.1,3),fn=ripple,hessian=TRUE)
-##' subplex(par=c(0.1,3,2),fn=ripple,hessian=TRUE)
-##'
-##' rosen <- function (x, g = 0, h = 0) {   ## Rosenbrock Banana function (using names)
-##'   x1 <- x['a']
-##'   x2 <- x['b']-h
-##'   100*(x2-x1*x1)^2+(1-x1)^2+g
-##' }
-##' subplex(par=c(b=11,a=-33),fn=rosen,h=22,control=list(abstol=1e-9,parscale=5),hessian=TRUE)
+##' fit <- subplex(par=c(11,-33),fn=rosen)
+##' stopifnot(all.equal(fit$par,c(1,1),tol=1e-4))
+##' })
 ##'
 ##' @export subplex
 subplex <- function (par, fn, control = list(), hessian = FALSE, ...) {
@@ -88,7 +70,7 @@ subplex <- function (par, fn, control = list(), hessian = FALSE, ...) {
   fn <- match.fun(fn)
   tryCatch(
     .Call(
-      call_subplex,
+      C_subplex,
       par,
       fn,
       tol=con$reltol,
